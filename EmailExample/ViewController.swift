d913342e7e8ade298913c801affcf51697e8fd7e
emily-byrne <ecbyrne@asu.edu>
2017-09-18 16:40:53 -0700

    Initial Commit

---
+++ EmailExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmailExample
+//
+//  Created by Emily Byrne on 9/18/17.
+//  Copyright © 2017 Byrne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

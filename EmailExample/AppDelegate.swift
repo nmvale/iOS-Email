c8d59f4150ca8a36adf9ea3276259577f3dcd833
emily-byrne <ecbyrne@asu.edu>
2017-09-21 15:37:11 -0700

    Start of homework

--- EmailExample/AppDelegate.swift
+++ EmailExample/AppDelegate.swift
@@ -21,8 +21,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //NEED: access to datapool object
         let splitVC = self.window?.rootViewController as! UISplitViewController
         let navVC = splitVC.viewControllers.first as! UINavigationController
-        let rootTVC = navVC.viewControllers[0] as! RootTVC
+        let menuTVC = navVC.viewControllers[0] as! MenuTVC
         
+        let test = Email(sender: "asu@asu.edu", subject: "Spam", contents: "Spam")
+        menuTVC.dataDictionary = ["Inbox":[Email(sender: "fellow.student@asu.edu", subject: "Project Question", contents: "Hi help me please")], "Sent":[Email(sender: "prof@asu.edu", subject: "Homework", contents: "Hi turn it in please")], "Trash":[Email(sender: "alert@asu.edu", subject: "Bees", contents: "Hi help me please there are bees")]]
+        
+        /*
         rootTVC.emails.append(Email(sender: "fellow.student@asu.edu", subject: "Project Question", contents: "Hi help me please"))
         rootTVC.emails.append(Email(sender: "prof@asu.edu", subject: "Homework", contents: "Hi turn it in please"))
         rootTVC.emails.append(Email(sender: "alert@asu.edu", subject: "Bees", contents: "Hi help me please there are bees"))
@@ -30,6 +34,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let detailVC = splitVC.viewControllers.last as! ViewController
         
         rootTVC.delegate = detailVC
+ */
         
         return true
     }
